0d0f5f7c5e5f8b531304c60bd1e9545b520cb2d9
youssef <hmamoucheyussef@gmail.com>
2020-01-10 12:27:13 +0100

    update execution file

--- models_evaluation.sh
+++ models_evaluation.sh
@@ -1,1 +1,1 @@
-python evaluation.py -s 2 3 5 6 7 8 9 10 11 13 15 17 18 20 21 22 23 24 -rg 1 2 -p 6 -t pred #-crossv #-rm 
+python evaluation.py -s 1 2 3 5 6 7 8 9 10 11 15 17 18 20 21 22 23 24 25 -rg 1 2 3 4 5 6 -p 6 -t pred -rm #-crossv
